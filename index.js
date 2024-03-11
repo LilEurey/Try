1fab1af9a1f58b232e4c7380865d1f5499dbfefd
LilEurey <134917527+LilEurey@users.noreply.github.com>
2024-03-11 13:47:24 +0700

    Sixth changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let a= "Euro";
+let a = "Euro";
 console.log(a);
-console.log(5+5);
-console.log(6*6);
-console.log(7*7);
-console.log(8*8);
+console.log(5 + 5);
+console.log(6 * 6);
+console.log(7 * 7);
+console.log(8 * 8);
 console.log("Hello world");
-
+console.log("Euro");
